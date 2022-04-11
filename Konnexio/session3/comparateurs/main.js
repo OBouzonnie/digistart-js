d77cd07b6b42a743aa9ea3676e9147bc1a12ece2
Fley <florian@abtasty.com>
2022-04-11 21:21:52 +0200

    cours lundi 11 avril 2022

--- Konnexio/session3/comparateurs/main.js
+++ Konnexio/session3/comparateurs/main.js
@@ -2,41 +2,40 @@
 console.log('Bienvenue dans la console javascript');
 console.log('Vous êtes dans les exercices sur les comparateurs');
 
-// ------ TODO 1 ------ 
+// ------ TODO 1 ------
 // 1. Créer une variable neighbourAge qui contient l'âge de votre voisin
 
-
-
+var neighbourAge = 19;
 
 // 2. Créer une variable myAge qui contient votre âge
 
-
-
+var myAge = 20;
 
 // 3. Comparez les variables neighbourAge et monAge en utilisant des comparateurs (<, <=, >, >=, ===, !== ...)
 // puis affichez le résultat des comparaisons dans la console avec la méthode "console.log".
 
+console.log(neighbourAge < myAge);
 
-
-
-
-// ------ TODO 2 ------ 
+// ------ TODO 2 ------
 // 1. Créer une fonction showGreaterNumber qui prend en paramètre deux nombres et
 // qui affiche le plus grand des deux dans la console.
 
-
-
-
 // 2. L'exécuter avec les paramètres de votre choix.
 
-
-
-
-// ------ TODO 3 ------ 
+// ------ TODO 3 ------
 // 1. Créer une fonction isBetween1000And3000 qui prend en paramètre une variable et
 // qui renvoie un booléen indiquant si la variable est un nombre compris entre 1000 et 3000.
 
+function isGreaterthan1000(a) {
+  console.log(a > 1000);
+}
 
-
+function isBetween1000And3000(num) {
+  // Num plus petit que 3000 ET num est plus grand que 1000
+  return num < 3000 && num > 1000;
+}
 
 // 2. L'exécuter avec le paramètre de votre choix.
+console.log(isBetween1000And3000(3));
+console.log(isBetween1000And3000(1050));
+console.log(isBetween1000And3000(4000));
--- reezy971/session2/methodes/main.js
+++ reezy971/session2/methodes/main.js
@@ -1,55 +1,41 @@
 /* LES MÉTHODES */
-console.log("Bienvenue dans la console javascript");
-console.log("Vous êtes dans les exercices sur les méthodes");
+console.log('Bienvenue dans la console javascript');
+console.log('Vous êtes dans les exercices sur les méthodes');
 
-// ------ TODO 1 ------ 
+// ------ TODO 1 ------
 // TODO: ÉTAPE 1
 // Écrire une variable du nom de votre choix, et de valeur de type String.
-
-
-
+var myName = 'Aymeric';
 
 // TODO: ÉTAPE 2
 // Afficher sa longueur dans la console grâce à la propriété "length".
+console.log(myName.length);
 
-
-
-
-// ------ TODO 2 ------ 
+// ------ TODO 2 ------
 // TODO: ÉTAPE 1
 // Ajouter un espace au début ou à la fin de votre string.
-
-
-
-
+myName = ' ' + myName;
 
 // TODO: ÉTAPE 2
 // Afficher sa longueur dans la console, et observez la différence.
+console.log(myName.length);
 
-
-
-
-// ------ TODO 3 ------ 
+// ------ TODO 3 ------
 // TODO: ÉTAPE 1
 // Écrire une nouvelle variable du nom de votre choix, et de valeur de type String.
-
-
-
-
+var title = 'Reezy';
 // TODO: ÉTAPE 2
 // Afficher dans la console sa valeur en MAJUSCULES grâce à la méthode "toUpperCase".
-
-
-
-
-// ------ TODO 4 ------ 
+console.log(title.toUpperCase());
+// ------ TODO 4 ------
 // TODO: ÉTAPE 1
 // Créer une fonction convertToUppercase qui prend en paramètre une chaîne
 // de caractères et qui la renvoie en majuscule grâce à la méthode "toUpperCase".
-
-
-
-
-
+function convertToUppercase(str) {
+  return str.toUpperCase();
+}
 // TODO: ÉTAPE 2
 // L'exécuter avec le paramètre de votre choix.
+var upperStr = convertToUppercase('Reezy');
+console.log(upperStr);
+console.log(convertToUppercase(title));
